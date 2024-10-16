54f85b00f0eafb17443549bc6ffbbebafacce163
Spencer Barr-Redmond <spencer3350@hotmail.com>
2024-10-16 12:17:55 -0400

    new

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event =>{
     getVisitCount();
 }))
-
-const functionAPI = "http://localhost:7071/api/GetResumeCount";
+const functionAPIUrl = "https://rcoresume.azurewebsites.net/api/GetResumeCount?code=bZsy3sg5MhfTJtDBVLyDaAOU630W0qsP2ZjqUIQ_SPvVAzFut_Gt3g%3D%3D"
+const localfunctionAPI = "http://localhost:7071/api/GetResumeCount";
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionAPI).then(Response => {
+    fetch(functionAPIUrl).then(Response => {
         return Response.json()
     }).then(response => {
         console.log("Website called function API.");
