fcd957fa2882099ebaf6b6b4882f5091666696c8
Spencer Barr-Redmond <spencer3350@hotmail.com>
2024-10-16 10:20:08 -0400

    created azure function api

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event =>{
+    getVisitCount();
+}))
+
+const functionAPI = "http://localhost:7071/api/GetResumeCount";
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionAPI).then(Response => {
+        return Response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("rcoresumeCounter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
